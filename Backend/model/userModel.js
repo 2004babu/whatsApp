8ac443127b5db8c61d403960c4f144cd9fc00428
babu20041509 <bb5696359@gmail.com>
2024-07-09 09:42:17 +0530

    change userModel gender default others

--- Backend/model/userModel.js
+++ Backend/model/userModel.js
@@ -19,6 +19,7 @@ const userSchema = new mongoose.Schema(
       type: String,
       required: [true, "Gender is required"],
       enum: ["Male", "Female"],
+      default:'other'
     },
     password: {
       type: String,
